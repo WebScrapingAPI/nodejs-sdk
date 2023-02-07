563a6ca5e13ac4c658d3cb790660df9481932607
Mihnea Manolache <mihnea.dev@gmail.com>
2023-02-07 16:09:11 +0200

    switch from axios to got

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-const axios = require('axios');
-
 class WebScrapingApiClient {
     constructor(api_key) {
         this.api_key = api_key;
         this.api_url = "https://api.webscrapingapi.com/v1";
     }
-    request(method, url, params = {}, headers = {}, data = {}) {
+    async #request(method, url, params = {}, headers = {}, data = {}) {
+        const {got} = await import('got')
+        const {Options} = await import('got')
+
         params['url'] = url;
         params['api_key'] = this.api_key;
 
@@ -16,23 +17,28 @@ class WebScrapingApiClient {
         let final_url = this.api_url + '?' + queryString;
 
         return new Promise(resolve => {
-            axios({
-                method: method,
-                url: final_url,
-                data: data,
-                headers: headers,
-            }).then(function (response){
-                resolve({success: true, response: response});
-            }).catch(function (error) {
-                resolve({success: false, error: error});
-            })
+            let options = new Options()
+            options.method = method
+            options.headers = headers
+            options.maxHeaderSize = 100000
+            if ( method.toLowerCase() !== 'get' )
+                typeof data === 'object' 
+                    ? options.json = data 
+                    : options.body = data 
+            got(final_url, undefined, options)
+                .then((res) => {
+                    resolve({success: true, response: res})
+                })
+                .catch((e) => {
+                    resolve({success: false, error: e});
+                })
         });
     }
     async get(url, params = {}, headers = {}) {
-        return await this.request('GET', url, params, headers, {});
+        return await this.#request('GET', url, params, headers, {});
     }
     async post(url, params = {}, headers = {}, data={}) {
-        return await this.request('POST', url, params, headers, data);
+        return await this.#request('POST', url, params, headers, data);
     }
 }
 
